5320cb50928f4b9b6b3899973aa286d8d05eda36
batu <ba921@nyu.edu>
2019-01-09 19:06:47 -0800

    Added in the first sprite and movemement.

---
+++ Project_1/Mourning2019/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    PlayerCharacterController2D controller;
+
+    [Header("Player movement values.")]
+    public float runSpeed = 40f;
+
+    // Movement values
+    float horizontalMovement = 0f;
+
+
+    bool hasJumped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AssignComponents();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement  = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump")) {
+            hasJumped = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        controller.Move(horizontalMovement * Time.deltaTime, false, hasJumped);
+        hasJumped = false;
+    }
+
+    void AssignComponents() {
+        controller = GetComponent<PlayerCharacterController2D>();
+    }
+}
