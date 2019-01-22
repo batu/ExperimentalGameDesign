078bf27d8b27d4eb1d9a97754cf64564ed425a39
batu <ba921@nyu.edu>
2019-01-21 18:50:46 -0800

    Almost done with the scale

---
+++ Project_1/Mourning2019/Assets/Holder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holder : MonoBehaviour
+{
+
+    public int weight = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.name == "Player"){
+            weight += 1;
+        }
+        if (collision.gameObject.name == "SmallWeight") {
+            weight += 3;
+        }
+
+        if (collision.gameObject.name == "BigWeight") {
+            weight += 6;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.name == "Player") {
+            weight -= 1;
+        }
+        if (collision.gameObject.name == "SmallWeight") {
+            weight -= 3;
+        }
+        if (collision.gameObject.name == "BigWeight") {
+            weight -= 6;
+        }
+    }
+}
---
+++ Project_1/Mourning2019/Assets/Scale.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int totalWeight = 0;
+    public Holder holder1;
+    public Holder holder2;
+
+    bool notStarted = true;
+    void Start()
+    {
+        
+    }
+
+    bool isBalanced() {
+        return (holder1.weight - holder2.weight == 0 && holder1.weight != 0 && holder2.weight != 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBalanced() && notStarted) {
+            notStarted = false;
+            StartCoroutine(StablizeTheScales());
+        }  
+    }
+
+    IEnumerator StablizeTheScales() {
+        yield return null;
+    }
+}
